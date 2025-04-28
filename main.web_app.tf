e84742c6e24aecc9b10776f35c888f60587965f9
Espen Hermansen <espen.hermansen@gmail.com>
2025-04-28 13:58:45 +0200

    Update main.web_app.tf

--- main.web_app.tf
+++ main.web_app.tf
@@ -17,6 +17,9 @@ resource "azurerm_windows_web_app" "this" {
   public_network_access_enabled                  = var.public_network_access_enabled
   tags                                           = var.tags
   virtual_network_subnet_id                      = var.virtual_network_subnet_id
+  virtual_network_backup_restore_enabled         = true
+  vnet_image_pull_enabled                        = true
+
   webdeploy_publish_basic_authentication_enabled = var.site_config.ftps_state == "Disabled" ? false : var.webdeploy_publish_basic_authentication_enabled
   zip_deploy_file                                = var.zip_deploy_file
 
